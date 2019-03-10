b7d62da73c8af047be7e6b82a8d08e7223b0d8b7
Clouding <ghost0436@gmail.com>
2019-03-10 23:21:12 +0800

    Add system path.zsh

---
+++ system/path.zsh
@@ -0,0 +1,6 @@
+export PATH="/usr/local/sbin:$PATH"
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export EDITOR=vim
+export BAT_THEME=gruvbox-dark-medium
+export FZF_DEFAULT_OPTS='--bind alt-j:down,alt-k:up'
