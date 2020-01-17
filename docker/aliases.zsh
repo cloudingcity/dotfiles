d2f6d112ab3a452628c3fdee683d6790a4694027
clouding <ghost0436@gmail.com>
2020-01-18 00:22:03 +0800

    Update docker alias

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,9 +1,10 @@
-alias dp="docker ps"        # Get container process
-alias dpa="docker ps -a"    # Get process included stop container
-alias di="docker images"    # Get images
-alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'" # Get container IP
+alias dp="docker ps"        
+alias dpa="docker ps -a"   
+alias di="docker images"  
+alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'" 
 alias dr="docker run"
 alias drm="docker rm"
 alias drmi="docker rmi"
 alias de="docker exec"
+alias dco="docker-compose"
 dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
