44b711ed99010695a37be90921105a3d0356986e
clouding <ghost0436@gmail.com>
2021-01-01 20:14:30 +0800

    remove unused alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,6 +1,4 @@
 alias reload!='. ~/.zshrc'
-alias cls='clear' # Good 'ol Clear Screen command
-alias brewup='brew update; brew upgrade; brew prune; brew cleanup; brew doctor'
 alias zshconfig='vim ~/.zshrc'
 alias vimconfig='vim ~/.vimrc'
 alias desk='cd ~/Desktop'
