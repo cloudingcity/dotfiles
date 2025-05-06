d4b9901637ba7803b33042760c2702b328cc96c0
clouding <ghost0436@gmail.com>
2025-05-06 11:05:24 +0800

    chore: disable powerline

--- powerline/init.zsh
+++ powerline/init.zsh
@@ -1,5 +1,5 @@
-POWERLINE_SCRIPT=$HOME/Library/Python/3.9/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
-if [ -f $POWERLINE_SCRIPT ]; then
-    powerline-daemon -q
-    source $POWERLINE_SCRIPT
-fi
+# POWERLINE_SCRIPT=$HOME/Library/Python/3.9/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
+# if [ -f $POWERLINE_SCRIPT ]; then
+#     powerline-daemon -q
+#     source $POWERLINE_SCRIPT
+# fi
