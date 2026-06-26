46fc9054ab32c8e4725cfe04661ec098e3f02a59
clouding <ghost0436@gmail.com>
2026-06-26 10:41:17 +0800

    feat(powerline): remove configuration
    Remove Powerline color configurations, colorschemes, and themes for shell and tmux, delete the Powerline initialization script for zsh, and remove the link_powerline_config function and call from the bootstrap script.

--- powerline/init.zsh
+++
@@ -1,5 +0,0 @@
-# POWERLINE_SCRIPT=$HOME/Library/Python/3.9/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
-# if [ -f $POWERLINE_SCRIPT ]; then
-#     powerline-daemon -q
-#     source $POWERLINE_SCRIPT
-# fi
