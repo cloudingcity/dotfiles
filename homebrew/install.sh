b03d0f02f3fd91c3563b73279368c3cded41fb52
clouding <ghost0436@gmail.com>
2026-06-26 10:31:23 +0800

    feat(install): update brew and zsh installers
    Update the Homebrew installation command to the official unified Bash script and add its bin paths to the current environment using brew shellenv. Update the Oh My Zsh installation URL to the ohmyzsh/ohmyzsh repository.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,24 +1,17 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
-
 # Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
+if ! command -v brew >/dev/null 2>&1; then
+  echo "  Installing Homebrew for you..."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  # Use the official unified Bash installation command (applicable to both macOS and Linux)
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # Add Homebrew to the current shell environment so subsequent setup scripts can use 'brew' immediately
+  if [ -d "/opt/homebrew/bin" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [ -d "/home/linuxbrew/.linuxbrew/bin" ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   fi
-
 fi
 
 exit 0
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
